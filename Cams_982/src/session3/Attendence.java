9268a4f28707c2549a8762e13917f032d16d4322
Charan <charankn69@gmail.com>
2026-09-13 11:57:41 +0530

    Attendence

---
+++ Cams_982/src/session3/Attendence.java
@@ -0,0 +1,19 @@
+package session3;
+
+public class Attendence {
+	  public static void main(String[] args) {
+		    long attendanceId = 10100L;
+	        String date = "10-08-2026";
+	        String status = "Present";
+	        int totalClasses = 50;
+	        int classesAttended = 45;
+	        float attendancePercentage = 90.0f;
+	        
+	        System.out.println("Attendence is : " + attendanceId);
+	        System.out.println("Date is : "+ date);
+	        System.out.println("Staus is : " + status);
+	        System.out.println(totalClasses);
+	        System.out.println(classesAttended);
+	        System.out.println(attendancePercentage);
+	  }
+}
